d44258248e57aaff94b11148056b95edd700e257
saranvoleti <saranvoleti@gmail.com>
2017-04-01 14:53:55 -0500

    Update viewmodel.js

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,13 +1,20 @@
-function inintMap() {
 
-    var locations = [
+var locations = [
       {title: 'Park Ave Penthouse', position: {lat: 40.7713024,  lng: -73.9632393}, content: "Hello"},
       {title: 'Chelsea Loft', position: {lat: 40.7444883,  lng: -73.9949465}, content: "Hello1"},
       {title: 'Union Square Open Floor Plan', position: {lat: 40.7347062,  lng:  -73.9895759}, content: "Hello2"}
   ];
 
+  var Place = function (place){
+    this.title = place.title;
+    this.position = place.position
+    this.marker = place.marker
+    this.showlocation = ko.observable(true)
+  }
 
+  var vm = {};
 
+function inintMap() {
 
   var map;
   var bounds = new google.maps.LatLngBounds();
@@ -45,17 +52,25 @@ function inintMap() {
   google.maps.event.addListener(marker, "click", (function(marker) {
  return function(evt) {
 
- foursquareUrl = 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=32TBNUA4CCALYDULE0TLYPZPLLPJ2M3FQUOAK3Q05GXXWCNX&client_secret=C5D4DGFJMWJKDLZNCR1Q1T5OMKU41ZE23DRRQSNQ3KRP5JV4&v=20170309';
- $.getJSON(foursquareUrl)
-  .done(function(data){
+ foursquareUrl = 'https://api.foursquare.com/v2/venues/search?ll=40.763570,-73.972335&client_id=32TBNUA4CCALYDULE0TLYPZPLLPJ2M3FQUOAK3Q05GXXWCNX&client_secret=C5D4DGFJMWJKDLZNCR1Q1T5OMKU41ZE23DRRQSNQ3KRP5JV4&v=20170309';
+ $.ajax({
 
-     var placeName = data.response.name;
+    url : foursquareUrl,
+    dataType : 'json',
+    data: {
 
-   windowContent = '<div id="iw_container"><p><strong>Name: </strong>' + placeName+ '</p>'
+        async : true
 
 
-  }).fail(function(error){
-    infoWindow.setContent('Fail to connect to Foursquare: ' + error);
+  },
+
+  success : function (data){
+    console.log(data);
+  }
+
+
+
+
   }
   );
    }
@@ -86,28 +101,37 @@ function inintMap() {
 
 
 
-vm.locations = ko.observable('');
-vm.search1 = function(value) {
-  for (i = 0; i < locations.length; i++) {
-     var title = locations[i].title;
- vm.locations()[i].showLocation = ko.observable(true);}
-     vm.locations.push(title)
+vm.locationslist = ko.observableArray([]);
+// vm.filteredlocations = function (){
+    locations.forEach (function(place){
+        vm.locationslist.push(new Place(place))
+
+    })
+
+
+
+// vm.search1 = function(value) {
+//   for (i = 0; i < locationslist().length; i++) {
+//      var title = locations[i].title;
+//  vm.locations()[i].showLocation = ko.observable(true);}
+//      vm.locations.push(title)
 
 
 
 
   vm.query = ko.observable('');
 
-  vm.search = function(value) {
-    var value = value.toLowerCase();
-    var tempLocations = [];
-    for (var x in vm.locations()) {
-      var title = vm.locations()[x].title.toLowerCase();
+  vm.search = function() {
+    var value = vm.query().toLowerCase();
+    // var tempLocations = [];
+    for (var x in vm.locationslist()) {
+      var title = vm.locationslist()[x].title.toLowerCase();
       var match = title.indexOf(value) >= 0; // true or false
 
       console.log(title, value, match);
 
-       vm.locations()[x].marker.setVisible(match);
+       vm.locationslist()[x].marker.setVisible(match);
+       vm.locationslist()[x].showlocation(match);
     }
   }
 
@@ -118,5 +142,6 @@ vm.search1 = function(value) {
   ko.applyBindings(vm);
 
 
-}}
+
+}
 
